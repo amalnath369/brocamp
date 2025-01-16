b4a711b56c90e873a8addca5f34bf7b8d11d0e25
Amalnath <amalnaths640@gmail.com>
2025-01-16 21:07:36 +0530

    Updated styles and fixed bugs

--- brocamp/settings.py
+++ brocamp/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'brocamp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'amal',
+        'HOST': 'localhost',  # 'localhost' or remote PostgreSQL server
+        'PORT': '5432', 
     }
 }
 
